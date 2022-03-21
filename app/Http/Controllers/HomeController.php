5f951f3f3be1ebbf02d72304b3cadaad4be1b68c
Thinh <thinhtran23601@gmail.com>
2022-03-21 20:04:03 +0700

    Xuat du lieu db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 // use App\Helpers\CartHelper;
 use Validator;
@@ -11,12 +11,16 @@ class HomeController extends Controller {
         // Config page
         $title = "Trang chủ";
         // Data
-        
+        $category = DB::select('select * from category');
+        $products = DB::select('select categoryName, productCompany, productName, productImage1, productImage2, productImage3, productPrice, productPriceBeforeDiscount where category.id and products.category');
+
         // Handle
 
         // Return
         return view('home', [
             'title' => $title,
+            'category' => $category,
+            'products' => $products,
         ]);
     }
 }
