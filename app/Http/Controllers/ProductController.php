fa2a2da2333a79362a4745e0101fe4b0c73f77e0
Admin <hungln006@gmail.com>
2022-03-30 10:35:01 +0700

    m

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,7 @@
     
 namespace App\Http\Controllers;
 use App\Models\Products;
-use App\Services\CartHelper\CartHelper;
+use App\Services\CartHelper;
 use Illuminate\Http\Request;
 
 class ProductController  extends Controller {
